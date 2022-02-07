1166e29b4ddd860cb685cc4ab2e0210d327e443c
Marta Millan Lombraña <marta.millan@iesjoandaustria.org>
2022-02-07 12:36:48 +0100

    first commit

---
+++ app/src/main/java/com/example/examenm82022/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.examenm82022;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
+    }
+}
